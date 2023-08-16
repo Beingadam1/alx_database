44abcb35e5c668fc6965780d5094af28ef78c620
Babatunde Adam <adamlanre224@gmail.com>
2023-08-16 13:31:54 +0100

    edit comment

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
--- Write a script that creates a table called first_table in the current database in your MySQL server.
+-- Write a script that creates a table called first_table 
+-- in the current database in your MySQL server.
 -- first_table description:
 -- id INT
 -- name VARCHAR(256)
